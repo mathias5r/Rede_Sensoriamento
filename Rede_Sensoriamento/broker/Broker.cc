fbe05eaba8a3ce7f2fe88de959eb94f9878da1b6
ronyjah <ronyjah@gmail.com>
2016-12-14 18:26:08 -0200

    Add files via upload

--- Rede_Sensoriamento/broker/Broker.cc
+++ Rede_Sensoriamento/broker/Broker.cc
@@ -7,9 +7,9 @@
 #include "Broker.h"
 #include <pthread.h>
 
-using namespace std;
+ using namespace std;
 
-Broker::Broker() {
+ Broker::Broker() {
     Top[0].setAssunto("1.1");
     Top[1].setAssunto("1.2");
     Top[2].setAssunto("1.3");
@@ -19,178 +19,119 @@ Broker::Broker() {
 
 void Broker::sendNotify(string addr, string  valor, string assOID, int port,Connection * s)
 {
- cout << "-----------Entrou no notify--------:"<< endl;
- cout << "Valor da informaçao"<< valor << endl;
-    TAtivo pkt;
-    TAtivo::Choice_id & id = pkt.get_id();
-    TNotify da = id.get_notify();
-    da.set_hierarchy(assOID);
-    char * aux = new char[10];
-    //sprintf(aux,"%s",valor);
-    da.set_data(valor);
-    pkt.check_constraints();
-    pkt.show();
-    ostringstream output;
-    TAtivo::XerSerializer encoder(output);
-    encoder.serialize(pkt);
-    string data = output.str();
-    cout << "-----------Envia notify--------:"<< endl;
-    //TCPClientSocket sockNotify(addr,port);
-    //sockNotify.connect();
-    try{
-        cout << "Notify: " << s->send(data) << " bytes de " << data.size() << endl;
-        
-        //cout << "Data: " << sockNotify.send(data) << " bytes de " << data.size() << endl;
+   TAtivo pkt;
+   TAtivo::Choice_id & id = pkt.get_id();
+   TNotify da = id.get_notify();
+   da.set_hierarchy(assOID);
+   char * aux = new char[10];
+   da.set_data(valor);
+   pkt.check_constraints();
+   pkt.show();
+   ostringstream output;
+   TAtivo::XerSerializer encoder(output);
+   encoder.serialize(pkt);
+   string data = output.str();
+
+   try{
+    cout << "Notify: " << s->send(data) << " bytes de " << data.size() << endl;
     }catch (TCPServerSocket::DisconnectedException e) {
         cout << e.what() << ": " << e.get_addr() << ':';
         cout << e.get_port()<< endl;
     }
-    //sockNotify.close();
 }
 
 
 
 void Broker::recvPublish(string addr,string assunto, string informacao){
-    cout << "Entrou no recvPublish" << endl;
     for(int i =0; i < this->idTopicos; i++){
-        cout << "valor de assunto" << assunto << endl;
-        cout << "Valor de consultaAssunto"<< Top[i].consultaAssunto() << endl;
         if(this->Top[i].consultaAssunto() == assunto){
-            cout << "Assunto cadastrado no topico"<< i << endl;
-            cout << "Tamanho da fila de IPs"<< Top[i].endIP.size() << endl;
             int idIP = Top[i].endIP.size();
-            for(int ii = 0;ii < idIP;ii++)
-                sendNotify(Top[i].endIP[ii],informacao,assunto,Top[i].porta[ii],Top[i].si[ii]);
-
-                //sendNotify(this->Top[i].endIP[ii],&informacao,assunto);
-            }
+            for(int ii = 0;ii < idIP;ii++) sendNotify(Top[i].endIP[ii],informacao,assunto,Top[i].porta[ii],Top[i].si[ii]);
         }
     }
+}
 
-//void Broker::recvSubscriber(string addr, string issue, int port, TCPServerSocket & s){
 void Broker::recvSubscriber(string addr, string issue, int port, Connection * s){
-//void Broker::recvSubscriber(string addr, int tipo, string issue, string port){
-    //int i = this->idTopicos;
     for ( int i=0; i < this->idTopicos; i++)
     {
         if(this->Top[i].consultaAssunto() == issue)
         {
-            cout << "cadastrado participante no topico"<< i << endl;
             Top[i].endIP.insert(Top[i].endIP.end(),addr);
             Top[i].porta.insert(Top[i].porta.end(),port);
             Top[i].si.insert(Top[i].si.end(),s);
-            cout << "Tamanho da fila de IPs"<< Top[i].endIP.size() << endl;
         }
     }
     return;
 }
 
-void Broker:: recvUnsubscriber(string addr, string issue){
-
+void Broker::recvUnsubscriber(string addr, string issue){
     for ( int i=0; i < this->idTopicos; i++)
     {
         if(this->Top[i].consultaAssunto() == issue)
         {
             int j;
-            for( j=0 ; j < this->idTopicos ; i++ ){
+            for( j=0 ; j < Top[i].endIP.size() ; i++ ){
                 if( this->Top[i].endIP.at(j) == addr ){
-                    this->Top[i].endIP.erase(j);
-                    this->Top[i].porta.erase(j);
-                    this->Top[i].si.erase(j);
+                    this->Top[i].endIP.erase(this->Top[i].endIP.begin()+j);
+                    this->Top[i].porta.erase(this->Top[i].porta.begin()+j);
+                    this->Top[i].si.erase(this->Top[i].si.begin()+j);
                 }
             }
+        }
+    }
 }
-
 void Broker::recebePacote(TCPServerSocket * sock){
-    //void recebePacote(TCPClientSocket & socke);
-
- stringstream input;
+    stringstream input;
     TAtivo::XerDeserializer decoder(input);
-     while(true){
-    try{
+    while(true){
+        try{
          Connection & socke = sock->wait(0);
-        //
-        string addr;
-        unsigned short port;
-        socke.get_peer(addr, port);
-      
-        //string ip = sock.get_ip();
-        // conseguiu ler algo desse socket ...
-        string msg = socke.recv(1024);
-        if (socke.isNew()) {                
-                cout << "Nova conexão: " << addr << ':' << port << endl;
-
-            } else { 
-            
-              if (msg.size()) {                
-                // ...mostra-os na tela e envia-os de volta
-                // para a outra ponta da conexão
+         string addr;
+         unsigned short port;
+         socke.get_peer(addr, port);
+         string msg = socke.recv(1024);
+         if (socke.isNew()) {                
+            cout << "Nova conexão: " << addr << ':' << port << endl;
+        } else { 
+            if (msg.size()) {                
                 cout << "recebeu de " << addr << ':' << port;
                 cout << ": " << msg << endl;
- 
-                //msg = "recebido: " + msg;
-                //sock.send(data);
- 
-    input.write(msg.c_str(), msg.size());
-    TAtivo * other = decoder.deserialize();
-    TAtivo::Choice_id & id = other->get_id();
-    //falta testar se é do tipo connect.
-   
-    //string = 
-    cout << "Valor get_choice: " << id.get_choice() << endl;
-     //cout << "Valor bb antes: " << bb << endl;
-    int bb = id.get_choice();
-    //string topico = id.get_atop();
-    //string name = id.get_name();
-        cout << "Valor bb: " << bb << endl;
-    if ( bb == 1  ){ //publish
-        //eh um publish
-        cout << "Mensagem publish: " << endl;
-        TPublish co = id.get_publish();
-        //ASN1Oid assunto = co.get_hierarchy_attr();
-        string assunto  = co.get_hierarchy();
-        cout << "Valor assunto: " << assunto << endl;
-        string informacao = co.get_data();
-        cout << "Valor info: " << informacao << endl;
-        recvPublish(addr,assunto,informacao);
 
-
-    }else if(bb == 2){ //subscriber
-        //eh um subscriber;
-          cout << "Mensagem subscriber: " << endl;
-         TSubscriber ci = id.get_subscriber();
-         //cout << "Valor id.get_subscriber: " << id.get_subscriber() << endl;
-         cout << "Valor id.get_hierarchy: " << ci.get_hierarchy() << endl;
-         //ASN1Oid assunto = ci.get_hierarchy_attr();
-         string assunto = ci.get_hierarchy();
-
-        recvSubscriber(addr,assunto,port,&socke);
-        //recvSubscriber(addr,assunto,port,*sock);
-        
-    }else if(bb == 3){
-        //eh um unsubcriber
-        cout << "Mensagem UnSubscriber: " << endl;
-        TUnsubscriber ce = id.get_unsubscriber();
-        string assunto = ce.get_hierarchy();
-        recvUnSubscriber(addr,assunto);
-    }
-
-
-    }
-
-    cout << endl;
-
-    if (other) {
-        cout << "Estrutura de dados obtida da decodificação DER:" << endl;
-        other->show();
-    }
-
-    delete other;
-
-
-
-            }    
+                input.write(msg.c_str(), msg.size());
+                TAtivo * other = decoder.deserialize();
+                TAtivo::Choice_id & id = other->get_id();
+                int bb = id.get_choice();
+        if ( bb == 1  ){ //publish
+            cout << "Mensagem publish: " << endl;
+            TPublish co = id.get_publish();
+            string assunto  = co.get_hierarchy();
+            string informacao = co.get_data();
+            recvPublish(addr,assunto,informacao);
+        }else if(bb == 2){ //subscriber
+            cout << "Mensagem subscriber: " << endl;
+            TSubscriber ci = id.get_subscriber();
+            string assunto = ci.get_hierarchy();
+            recvSubscriber(addr,assunto,port,&socke);
+        }else if(bb == 4){
+            //eh um unsubcriber
+            cout << "Mensagem UnSubscriber: " << endl;
+            TUnsubscriber ce = id.get_unsubscriber();
+            string assunto = ce.get_hierarchy();
+            recvUnsubscriber(addr,assunto);
         }
+        cout << endl;
+
+        if (other) {
+            cout << "Estrutura de dados obtida da decodificação DER:" << endl;
+            other->show();
+        }
+
+        delete other;
+
+
+
+        }    
+    }
     }catch(TCPServerSocket::DisconnectedException e) {
         cout << e.what() << ": " << e.get_addr() << ':';
         cout << e.get_port()<< endl;
--- Rede_Sensoriamento/broker/demo.cc
+++ Rede_Sensoriamento/broker/demo.cc
@@ -15,112 +15,39 @@ void *task1(void *);
 
 int main() {
 
-    char * info = new char[12];
-    char * ip = new char[15];
-    info = (char*)"Test message";
-    ip = (char*)"127.0.0.1";
-    pthread_t threadA[3];
-    //TCPClientSocket tc(ip,2222);
     TCPServerSocket ts(3333);
     Broker bk;
-   ;
-
-    // Broker bk;
-    // bk.recebePacote(sock);
     int sock_fd = ts.get_descriptor();  
-    
-    //Connection co(sock_fd);
     int sock_fdco = 0;//co.get_descriptor();
+    int maior =  sock_fd;
 
-int maior =  sock_fd;
-if(sock_fdco > sock_fd){maior = sock_fdco;}else{maior = sock_fd;}
     while(1){
-            fd_set r;
-
-        // inicia o conjunto de descritores, e nele
-        // acrescenta fd1 e fd2
+        fd_set r;
         FD_ZERO(&r);
         FD_SET(sock_fd, &r);
         FD_SET(sock_fdco, &r);
-        //FD_SET(this->aplicacao, &r);
-    int n = 0;
-    if( !(n = select(maior+1, &r, 0, 0, 0)) == 0 ){
+        int n = 0;
+        try{
+            if( !(n = select(maior+1, &r, 0, 0, 0)) == 0 ){
+                cout << "INFO: Há " << n << " descritores prontos" << endl;
+                if(n < 0){
+                    perror("select()");
 
-            cout << "INFO: Há " << n << " descritores prontos" << endl;
-
-
-            if(n < 0){
-                perror("select()");
-
-            }else if(n){
-
-                // testa se fd1 está pronto para ser acessado
-                if (FD_ISSET(sock_fd, &r)) {
-                    cout << "NOVA CONEXÃO" << endl;
-                    Connection & sock = ts.wait(0);
-                    sock_fdco = sock.get_descriptor();
-                    if(sock_fdco > sock_fd){maior = sock_fdco;}else{maior = sock_fd;}
+                }else if(n){
+                    if (FD_ISSET(sock_fd, &r)) {
+                        Connection & sock = ts.wait(0);
+                        sock_fdco = sock.get_descriptor();
+                        if(sock_fdco > sock_fd){maior = sock_fdco;}else{maior = sock_fd;}
                         bk.recebePacote(&ts);  
-                    // this->arq.set_received(true);
-                    // this->arq.handle();
-                    // this->arq.set_received(false);
-                    // this->begin = clock();
-
-                }if(FD_ISSET(sock_fdco,&r)){
-                    cout << "Conexão Antiga" << endl;
-                     Connection & sock = ts.wait(0);
-                     bk.recebePacote(&ts); 
+                    }if(FD_ISSET(sock_fdco,&r)){
+                        Connection & sock = ts.wait(0);
+                        bk.recebePacote(&ts); 
+                    }
                 }
-        
-            cout << "Eu voltei, pois aqui é meu lugar!" << endl;
-
             }
-        }
-    }
-}   //while(1){};
-    
-
-//      int noThread = 0;
-
-//      while (noThread < 3)
-//      {
-//         Connection & sock = ts.wait(0);
-//         //Broker bk;
-//         //bk.recebePacote(ts);  
-// //         int i=0; 
-//          pthread_create(&threadA[noThread], 0, task1, (void*)&ts); 
-//          noThread++;
-//      }
-//      for(int i = 0; i < 3; i++)
-//      {
-//          pthread_join(threadA[i], 0);
-//      }
-//      return 0;
-// }
-
-void * task1 (void *dummyPt)
-{    
-   // TCPServerSocket & sock = (TCPServerSocket)dummyPt;
-    cout << "MSG "<< endl;
-    Broker bk;
-    bk.recebePacote((TCPServerSocket*)dummyPt);  
-    return 0;
-}
-//recebe publish do sensor; os assuntos estão registrados;
-
-
-
-    // this->tty_fd = open(path,O_RDWR|O_NOCTTY);
-    // if (tty_fd < 0) throw -10;
-    // config(this->tty_fd, rate);
-
-    // Tun iface("ptp0" , "192.168.1.2", "192.168.1.1");
-    // iface.start();
-    // }   Serial t("/dev/ttyUSB0",B9600);
-    // Framework f(t,1,FRAME_MAXSIZE);
-    // ARQ arq(f, iface);
-    // Dispacher d(arq,t.get_fd(),iface.get());
-
-    // int Tun::get() {
-    //  if (fd >= 0) return fd;
-    //  throw -3;
+        }catch (TCPServerSocket::DisconnectedException e) {
+                cout << e.what() << ": " << e.get_addr() << ':';
+                cout << e.get_port()<< endl;
+            }
+        }   
+    } 
