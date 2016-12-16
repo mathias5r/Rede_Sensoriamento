5a62e39adf69c488b646914e7cfd9e1642953dcb
mathias5r <mathiassilva4@gmail.com>
2016-12-16 18:17:56 -0200

    Adicionado comentários demo.cc

--- Rede_Sensoriamento/demo.cc
+++ Rede_Sensoriamento/demo.cc
@@ -11,6 +11,30 @@
 #include <fcntl.h>
 
 
+/*
+    O protocolo de aplicação é baseado no modelo publish/subscriber, sendo que o conteúdo da mensagem a ser transmitida é codificada
+    em ASN1++ conforme espeficicação do projeto. Os participantes podem ser tanto publish como subscriber, sendo que o subscribers
+    são notificados imediatamente a cada publish recebido. 
+    Pela complexidade de sincronismo entre Thread o projeto foi modificado, no que se refere a multiplas requisições de pub/sub,
+    para o modelo com select, através de descritores de arquivos, um para novas conexões e outra para antigas conexões.
+    O sock_fd é o descritor de arquivo para novas conexões e o sock_fdco para atuar em conexões já existentes.
+    As novas conexões de subscribers são armazenadas em um vetor de subs contido no objeto da classe tópico.
+
+    Elementos do projeto:
+ 
+    Broker: registrar os subscribers em tópicos e informa-los com os dados do respectivo tópico.
+    Participantes: Os participantes podem ser publish e/ou subscribers. A cada publish enviado, todos os subscribers registrados
+    no tópico do publish recebido, serão notificados.
+
+    O vocabulário do protocolo é composto por:
+ 
+    Subscriber: Anuncia ao Broker o intenção em se registrar em tópico.
+    Publish: Mensagem enviada ao broker anunciando um novo dado para um respectivo tópico.
+    Notify: Informa ao subscriber o novo dado recebido em um respectivo tópico.
+    Unsubcriber: Elimina um subscriber registrado em um específico tópico.
+*/
+
+
 void *task1(void *);
 
 int main() {
